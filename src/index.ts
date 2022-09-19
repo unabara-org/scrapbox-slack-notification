55bde4960d0e754bedde65198c5685e9d62d7a04
mirai shigematsu <miraishigematsu@mirainoMacBook-Pro.local>
2022-09-19 17:15:55 +0900

    add: scrapbox からの post があったら slack へ通知する処理をルーティングファイルに記載

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,46 @@
 import express from 'express';
+import { ScrapboxNotice } from './@types/ScrapboxNotice';
+import SlackPostOptions from './@types/SlackPostOptions';
+import ScrapboxService from './services/ScrapboxService';
+import SlackNoticeService from './services/SlackNoticeService';
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const app = express();
 const port = 8080;
 
+app.use(express.json());
+
 app.post('/push_to_slack', (req, res) => {
-  res.send('hello world');
+  if (typeof process.env.SLACK_API_TOKEN == null) {
+    throw new Error('SLACK_API_TOKEN の値がありません');
+  }
+  const scrapboxNoticeObj = req.body as ScrapboxNotice;
+
+  if (ScrapboxService().isNotifiable(scrapboxNoticeObj)) {
+    const token = process.env.SLACK_API_TOKEN as string;
+
+    let options: SlackPostOptions = {
+      channel: 'unabara_scrapbox',
+      text: scrapboxNoticeObj.text,
+      attachments: [],
+    };
+
+    for (const item of scrapboxNoticeObj.attachments) {
+      options.attachments.push({
+        title: item.title,
+        title_link: item.title_link,
+        text: item.text,
+        author_name: item.author_name,
+      });
+    }
+
+    SlackNoticeService().push(token, options);
+  }
+
+  res.send('ok');
 });
 
 app.listen(port, () => {
