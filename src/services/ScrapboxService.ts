91983062a845943fb13b3bc100684727543fff64
mirai shigematsu <mirai3.2.1993@gmail.com>
2023-02-09 15:25:41 +0900

    improve: isNotifiable の実装を古河くんの教えてくれた方法に変更

--- src/services/ScrapboxService.ts
+++ src/services/ScrapboxService.ts
@@ -7,10 +7,8 @@ export const ScrapboxService = () => {
   const isNotifiable = (attachment: ScrapboxNoticeAttachment): boolean => {
     const ignoreTitlePatterns = [/^日報[\s　]/, /^下書き[\s　]/];
 
-    return (
-      ignoreTitlePatterns.filter((regex) => {
-        return regex.test(attachment.title);
-      }).length === 0
+    return ignoreTitlePatterns.every(
+      (pattern) => !pattern.test(attachment.title)
     );
   };
 
